1f8a4ac21b16327293c1fa23a3880c79d264798b
Matvey Ryabchikov <35634442+ronanru@users.noreply.github.com>
2022-08-18 16:59:02 +0300

    demo site fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,11 @@
 <script lang="ts">
   import FlipClock from '$lib/FlipClock.svelte';
   import '../global.css';
-
-  let date = new Date();
-  date.setHours(10);
-  date.setMinutes(14);
-  date.setSeconds(2);
-
-  setInterval(() => date.setMinutes(Math.floor(Math.random() * 60)), 1000);
 </script>
 
 <h1>svelte-flip-clock demo</h1>
 <div class="demo">
-  <FlipClock bind:date size={2} />
+  <FlipClock size={2} />
 </div>
 <a href="https://github.com/ronanru/svelte-flip-clock">Documentation on GitHub</a>
 
--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,21 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+export default {
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: null,
+      precompress: false
+    }),
+    prerender: {
+      default: true
+    }
+  }
 };
-
-export default config;
